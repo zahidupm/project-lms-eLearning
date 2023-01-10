c85833be9a0866ab7501a6970d32025e834b336f
zahidupm <zahidhasanlimon111@gmail.com>
2023-01-11 00:59:46 +0600

    leads edit delete and view and pagination

---
+++ app/Http/Livewire/LeadIndex.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Lead;
+use Livewire\WithPagination;
+use Flasher\Prime\FlasherInterface;
+
+class LeadIndex extends Component
+{
+    public function render()
+    {
+        $leads = Lead::paginate(10);
+        return view('livewire.lead-index', [
+            'leads' => $leads
+        ]);
+    }
+
+    public function leadDelete($id, FlasherInterface $flasher) {
+        $lead = Lead::findOrFail($id);
+        $lead->delete();
+
+        flash()->addSuccess('Lead Deleted Successfully');
+    }
+}
