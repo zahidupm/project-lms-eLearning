d016b89c2456334fb4c36b67ba28125c57391560
zahidupm <zahidhasanlimon111@gmail.com>
2023-01-14 16:20:38 +0600

    how to creat user

--- app/Http/Livewire/UserCreate.php
+++ app/Http/Livewire/UserCreate.php
@@ -3,11 +3,46 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Flasher\Prime\FlasherInterface;
 
 class UserCreate extends Component
 {
+
+    public $name;
+    public $email;
+    public $password;
+    public $role;
+
+    protected $rules = [
+        'name' => 'required',
+        'email' => 'required|email|unique:users,email',
+        'password' => 'required|min:6',
+        'role' => 'required'
+    ];
+
     public function render()
     {
-        return view('livewire.user-create');
+        $roles = Role::all();
+        return view('livewire.user-create', [
+            'roles' => $roles
+        ]);
+    }
+
+    public function submitForm(FlasherInterface $flasher) {
+        $this->validate();
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+
+        $user->assignRole($this->role);
+
+        flash()->addSuccess('User created Successfully');
+        return redirect()->route('user.index');
     }
 }
